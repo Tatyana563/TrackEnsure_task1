56883bf95b794806949d1809da7132e4f58b8659
Tatyana563 <nikolaev.english@gmail.com>
2019-11-20 17:29:43 +0200

    test22

--- src/StringWork.java
+++ src/StringWork.java
@@ -4,20 +4,25 @@ public class StringWork {
         var8(s1);
     }
 
-    public static void var8(String str) {
+    public static String var8(String str) {
         String str2 = null;
         StringBuilder sb = new StringBuilder();
         System.out.println("Unmodified:" + str);
+       // sb.append(str.charAt(0));
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isLetterOrDigit(str.charAt(i))) {
-                sb.append(str.charAt(i));
-                str2 = sb.toString();
-                str2 = str2.replace("", " ").trim();
 
+            if (Character.isLetter(str.charAt(i))) {
+                sb.append(str.charAt(i));
             }
+               else if(Character.isWhitespace(str.charAt(i))&&
+                    (i==0||Character.isWhitespace(str.charAt(i-1))){
+                sb.append(str.charAt(i));
+            }
+            return sb.toString();
+
 
         }
-        System.out.println("Modified:" + str2);
+       
 
     }
 }
